89b8b2f9100feec6a887dc0e9284bb1f43cae466
Taimour <mukhay@ttu.ee>
2024-02-22 15:40:54 +0200

    Add constructor, use super

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,13 @@
-function Car() {
-  return <h2>Heyyyy, I am a Car!</h2>;
+import React from 'react';
+
+class Car extends React.Component {
+  constructor() {
+    super();  // your component has access to all the functions of the parent component (React.Component).
+    this.state = {color: "red"};
+  }
+  render() {
+    return <h2>I am a {this.state.color} Car!</h2>;
+  }
 }
 
 export default Car;
