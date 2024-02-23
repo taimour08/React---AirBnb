a30c25a414d01d919e5f0aa48cb7f40ae636ede5
Taimour <mukhay@ttu.ee>
2024-02-23 10:53:49 +0200

    node: read and display content from html file

--- Node App/demo_querystring.js
+++ Node App/demo_querystring.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var url = require('url');
-
+var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
-}).listen(8081);
+  fs.readFile('demo_html.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
