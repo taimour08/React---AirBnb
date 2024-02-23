cec31c0003045777cf28b219bacde5b562b02464
Taimour <mukhay@ttu.ee>
2024-02-23 11:52:10 +0200

    node: parse a web url

--- Node App/demo_querystring.js
+++ Node App/demo_querystring.js
@@ -1,24 +1,10 @@
-var http = require('http');
-var fs = require('fs');
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
 
-  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
 
-  fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
-    if (err) throw err;
-    console.log('Replaced!');
-  });
-
-
-  fs.unlink('mynewfile3.txt', function (err) {
-    if (err) throw err;
-    console.log('File DelD')
-  });
-
-
-  fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-    if (err) throw err;
-    console.log('File Renamed!');
-  });
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata.month); //returns 'february'
