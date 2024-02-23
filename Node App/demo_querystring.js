645476d0503e4ecdf051d2bf54e570909dd79403
Taimour <mukhay@ttu.ee>
2024-02-23 12:21:08 +0200

    node: import and use uppercase module

--- Node App/demo_querystring.js
+++ Node App/demo_querystring.js
@@ -1,17 +1,13 @@
 var http = require('http');
-var url = require('url');
-var fs = require('fs');
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
+import('upper-case').then(uc => {
+  http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+    res.write(uc.upperCase("Hello World!"));
+    res.end();
+  }).listen(8080);
+  
+}).catch(err => {
+  // Handle any errors
+});
+
