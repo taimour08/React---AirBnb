e9d1b62097b62a2923896d2f57a3c903ae05eaa4
Taimour <mukhay@ttu.ee>
2024-02-23 11:09:02 +0200

    node: add writeFile()

--- Node App/demo_querystring.js
+++ Node App/demo_querystring.js
@@ -1,6 +1,19 @@
+var http = require('http');
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+    fs.open('mynewfile2.txt', 'w', function (err, file) {
+      if (err) throw err;
+      console.log('Saved!');  
+  });
+
+  var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
