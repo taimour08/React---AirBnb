fa47bcb2720c18d13810bd2342ec0e0985080b3f
Taimour <mukhay@ttu.ee>
2024-02-26 12:27:40 +0200

    react: build a photo gallery

--- src/index.js
+++ src/index.js
@@ -53,9 +53,41 @@ function Drumset() {
 }
 
 
+function PictureGallery({ pictures }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextPicture = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === pictures.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevPicture = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? pictures.length - 1 : prevIndex - 1));
+  };
+
+  return (
+    <div>
+      <button onClick={prevPicture}>Previous</button>
+      <img src={pictures[currentIndex]} alt={`Picture ${currentIndex}`} />
+      <button onClick={nextPicture}>Next</button>
+    </div>
+  );
+}
+
+function App() {
+  const pictures = [
+    'https://lh4.googleusercontent.com/-oF6Gtkjj_Q8/TX-lex6NltI/AAAAAAAABpo/aZezJr-sMn4/s1600/Yamaha-GigMaker-DrumSet2.jpg',
+    'https://d1aeri3ty3izns.cloudfront.net/media/6/66449/1200/preview.jpg',
+    'https://www.thefamouspeople.com/profiles/images/chris-martin-3.jpg',
+    // Add more picture URLs as needed
+  ];
+
+  return <PictureGallery pictures={pictures} />;
+}
+
+export default App;
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Drumset/>);
+root.render(<App/>);
